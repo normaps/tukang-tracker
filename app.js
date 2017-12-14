2440529e20643e0cb54d0f6853b477520e6b15d6
normaps <normapus1@gmail.com>
2017-12-15 00:56:24 +0700

    Add db uri

--- app.js
+++ app.js
@@ -5,6 +5,22 @@ const path = require('path')
 const http = require('http')
 const bodyParser = require('body-parser')
 const socketIo = require('socket.io')
+const { Client } = require('pg')
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
+
+// client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+//   if (err) throw err;
+//   for (let row of res.rows) {
+//     console.log(JSON.stringify(row));
+//   }
+//   client.end();
+// });
 
 const app = express()
 const server = http.createServer(app)
@@ -13,13 +29,9 @@ const io = socketIo(server)
 const locationMap = new Map()
 
 // Set the server port
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 8080
 
 app.use(express.static(path.join(__dirname, 'public')))
-
-app.get('/', (req, res) => {
-	res.send('Hello world')
-})
 
 io.on('connection', socket => {
 	locationMap.set(socket.id, {lat: null, lng: null})
@@ -41,24 +53,3 @@ server.listen(port, err => {
 	}
 	console.log('server started on port ' + port)
 })
-
-// const config = {
-// 	apiKey: "AIzaSyDBbrSoij9t2JcbXYbPFXWfFEGCwQLEj20",
-// 	authDomain: "tukang-tracker-186902.firebaseapp.com",
-// 	databaseURL: "https://tukang-tracker-186902.firebaseio.com",
-// 	projectId: "tukang-tracker-186902",
-// 	storageBucket: "tukang-tracker-186902.appspot.com",
-// 	messagingSenderId: "267471250627"
-// };
-
-// firebase.initializeApp(config);
-// // Get elements
-// const preObject = document.getElementById('object');
-
-// // Create preferences
-// const dbRefObject = firebase.database().ref().child('object');
-
-// // Sync object changes
-// dbRefObject.on('value', snap => {
-// 	preObject.innerText = JSON.stringify(snap.val(), null, 3);
-// });
