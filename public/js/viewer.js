ad6f866317a5f2d87d9ce1f93012c16cae6d8454
Intan Sari Septiana <intansrsptn@gmail.com>
2017-12-21 14:45:21 +0700

    search kuy

--- public/js/viewer.js
+++ public/js/viewer.js
@@ -151,6 +151,26 @@ function getLocation() {
         markersArray = [];
       }
 
+function searchTukang(keyword) {
+	let arr = [
+	    { id: ,name:"string 1", phone:"this", merchant_name: "that" },
+	    { id: ,name:"string 2", phone:"this", merchant_name: "that" }
+	];
+
+	let obj = arr.find(o => o.merchant_name === keyword);
+	console.log(obj);
+}
+
+function pickCategory(categoryInput) {
+	let arr = [
+	    { id: ,name:"string 1", phone:"this", merchant_name: "that", category: "makanan" },
+	    { id: ,name:"string 2", phone:"this", merchant_name: "that", category: "minuman" }
+	];
+
+	let obj = arr.find(o => o.category === categoryInput);
+	console.log(obj);
+}
+
 function getMerchantCategories() {
 	var request = new XMLHttpRequest();
 	request.open('GET', '/merchant_categories', true);
